52c49276b3968ba60765907d5ab67f805e7f2d32
Steve Raffner <jadeites_manteau_9c@icloud.com>
2025-05-05 10:18:04 +0200

    Update main.py
    rendu

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,26 @@
 def sum(a, b):
+    """
+    Calcule la somme de deux nombres.
+
+    Args:
+        a (int ou float): Le premier nombre.
+        b (int ou float): Le deuxième nombre.
+
+    Returns:
+        int ou float: La somme des deux nombres.
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """
+    Calcule la soustraction de deux nombres.
+
+    Args:
+        a (int ou float): Le premier nombre.
+        b (int ou float): Le deuxième nombre.
+
+    Returns:
+        int ou float: Le résultat de la soustraction (a - b).
+    """
     return a - b
