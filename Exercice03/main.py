a07a16b8d94337c4b1a37370caebd88099267842
Steve Raffner <jadeites_manteau_9c@icloud.com>
2025-05-05 10:17:01 +0200

    Update main.py
    exercice terminé

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,10 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+
+vowels = "aeiou"
+
+# Créer une liste de compréhension avec des tuples (mot, nombre de voyelles)
+word_vowel_counts = [(word, sum(1 for char in word if char in vowels)) for word in words]
+
+# Afficher la liste créée
+print(word_vowel_counts)
+
