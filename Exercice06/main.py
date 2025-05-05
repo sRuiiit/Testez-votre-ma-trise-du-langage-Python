1c5eeec7e157b8fcab06b757e5e6d94afe9af456
Steve Raffner <jadeites_manteau_9c@icloud.com>
2025-05-05 10:18:33 +0200

    Update main.py
    rendu

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,6 +1,10 @@
-# Fonction calculate_average
+def calculate_average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)# Fonction calculate_average
  
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
 print("La moyenne est :", average)
+
