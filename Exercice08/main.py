f5cb47e8c75f86a1fd7046aeabe61adcdfa89e74
Steve Raffner <jadeites_manteau_9c@icloud.com>
2025-05-05 10:19:31 +0200

    Update main.py
    fait

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,6 +1,19 @@
 def log_decorator(func):
-     pass
- 
+    """
+    Décorateur qui affiche un message avant et après l'exécution d'une fonction.
+
+    Args:
+        func (function): La fonction à enrouler.
+
+    Returns:
+        function: La fonction enroulée.
+    """
+    def wrapper():
+        print("Avant l'exécution de la fonction.")
+        func()
+        print("Après l'exécution de la fonction.")
+    return wrapper
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
